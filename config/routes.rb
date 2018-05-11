b8d7c67d7f90596a7252c52092a23cf770ba823c
Philipp Preuss <philipp.arian@gmail.com>
2018-05-11 11:44:35 +0800

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
 Rails.application.routes.draw do
-
- root 'tasks#index'
-
-#'read'
-
-get "tasks", to: "tasks#index"
-
-get 'tasks/new', to: 'tasks#new'
-
-get 'tasks/:id', to: 'tasks#show', as: :task
-
-post "tasks", to: "tasks#create"
-
-get "tasks/:id/edit", to: "tasks#edit", as: :tasks_edit
-
-patch "tasks/:id", to: "tasks#update"
-
-delete "tasks/:id", to: "tasks#destroy"
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
